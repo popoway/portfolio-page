554785582541a9b3f43f3feb5f6cdeebdec5a9cb
popoway <31576265+popoway@users.noreply.github.com>
2020-09-08 18:53:10 -0400

    feat: more content and dark mode css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,21 @@
 particlesJS.load('particles', 'assets/js/particles.json', function() {
-  console.log('callback - particles.js config loaded');
+  // console.log('particles.js config loaded');
+});
+
+function hack() {
+  console.log('%cHi there!', 'color:white; background:#439FEF; font-size: 16pt');
+  console.log('%cLove to hack? Connect with me on LinkedIn: ming.fyi/linkedin or Email me at popoway@popoway.cloud', 'font-size: 12pt');
+}
+
+function newTab() {
+  const anchor = document.querySelectorAll('a');
+  var a;
+  for (a of anchor) {
+    a.target = '_blank';
+  }
+}
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  newTab();
+  hack();
 });
