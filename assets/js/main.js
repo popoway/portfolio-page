2b602ea075d851f8cc66b4dc66c3c2fe55e49d32
Ming Lei <31576265+popoway@users.noreply.github.com>
2024-09-16 05:39:59 -0400

    feat: 2024 content

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ particlesJS.load('particles', 'assets/js/particles.json', function() {
 
 function hack() {
   console.log('%cHi there!', 'color:white; background:#439FEF; font-size: 16pt');
-  console.log('%cLove to hack? Connect with me on LinkedIn: ming.fyi/linkedin or Email me at hello@popoway.cloud', 'font-size: 12pt');
+  console.log('%cLove to hack? Connect with me on LinkedIn: ming.fyi/linkedin or Email me at ming@popoway.com', 'font-size: 12pt');
 }
 
 function newTab() {
